3ec32b46b02c538881e22c5f1d13dcc7772c2976
gheorgheandreea <gheorgheandreea03@yahoo.com>
2025-11-04 08:51:57 +0200

    Created the entities

---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,52 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "LicensePlate", nullable = false)
+    private String LicensePlate;
+    @Column(name = "parkingSpot", nullable = false)
+    private String parkingSpot;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getLicensePlate() {
+        return LicensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        LicensePlate = licensePlate;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
